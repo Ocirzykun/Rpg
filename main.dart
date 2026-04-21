8edcc248d10f94b3e409763b239e1fe1d6244e80
Ocirzykun <ocirzykunbetacura@gmail.com>
2026-04-21 14:20:53 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,11 +1,30 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Hello World</title>
-</head>
-<body>
+import 'character.dart';
+import 'warrior.dart';
+import 'archer.dart';
+import 'mage.dart';
+//import 'tank.dart';
+void performAttack(Character attacker, Character target){
+  attacker.attack(target);
+}
 
-<h1>Hello, World!</h1>
+void main(){
+  Warrior warrior = Warrior ("ROI", 100);
+  Archer archer = Archer ("JEMSON", 100);
+  Mage mage = Mage ("BOGART", 100);
+  //Tank tank = Tank ("KYM", 100);
 
-</body>
-</html>
+  
+  print("=====RPG CHARACTERS======");
+  print("${warrior .name} HP: ${warrior .health}");
+  print("${mage .name} HP: ${mage .health}");
+  print("${archer .name} HP: ${archer .health}");
+  //print("${tank .name} HP: ${tank .health}");
+
+
+  print("______________________________________\n");
+  performAttack(warrior, mage);
+  performAttack(mage, archer);
+  performAttack(archer, warrior);
+  //performAttack(tank, warrior);
+
+}
